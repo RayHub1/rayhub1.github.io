9bbc94367e44e82cb54d1e6fcd443ce93177b2b3
RayHub1 <m.raymondobot@gmail.com>
2023-09-19 11:54:06 -0700

    final commit

--- script.js
+++ script.js
@@ -14,17 +14,30 @@ $(document).ready(function () {
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
   });
-
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
+  });
+
+  $(".navbar .menu li a").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top,
+      },
+      800
+    ); // You can adjust the scroll speed (in milliseconds) here
+    // Close the menu if desired
+    $(".navbar .menu").removeClass("active");
+    $(".menu-btn i").removeClass("active");
   });
   //typing animation script
   var typed = new Typed(".typing", {
     strings: [
       "Petroleum Engineer",
-      "Web Developer",
       "Data Analyst",
+      "Web Developer",
       "Data Visualizer",
       "Freelancer",
     ],
